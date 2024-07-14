68b88fd281c5ad173c1dd780b4f86d26753ddbde
Shihab Kandil <shihab.mostafa1@gmail.com>
2024-07-15 01:03:36 +0300

    add agora token generation function

---
+++ README.md
@@ -0,0 +1,61 @@
+# Agora Users' authentication (rtc & rtm tokens generation) Cloud Function.
+
+## Prerequisites
+
+Before you begin, ensure you have the following installed:
+
+- [Node.js](https://nodejs.org/) and npm (Node Package Manager)
+- [Firebase CLI](https://firebase.google.com/docs/cli) installed globally
+
+## Getting Started
+
+Follow these steps to get started with the Firebase Cloud Functions:
+
+1. **Clone the Repository**: Clone this repository to your local machine.
+
+    ```bash
+    git clone https://github.com/shihabkandil/agora-token-function
+    cd functions
+    ```
+
+2. **Install Dependencies**: Install the required npm dependencies.
+
+    ```bash
+    npm install
+    ```
+
+3. **Set Up Firebase Project**: If you haven't already, initialize Firebase in your project and log in to your Firebase account.
+
+    ```bash
+    firebase login
+    ```
+
+    Initialize Firebase in your project (if not already initialized).
+
+    ```bash
+    firebase init functions
+    ```
+
+    This will set up your Firebase project to deploy cloud functions.
+
+4. **Environment Configuration**: Set up your environment variables. You can pass enviroment variables into your deploy command, make sure development account has Cloud Runtime Configuration API enabled. 
+    ```bash
+    firebase functions:config:set service.agora_app_id="AGORA_APP_ID"
+    firebase functions:config:set service.app_certificate="AGORA_APP_CERTIFICATE"
+    ```
+
+5. **Run Cloud Functions Locally**: Start the Firebase emulators to run functions locally.
+
+    ```bash
+    firebase emulators:start
+    ```
+
+6. **Deploy to Firebase**: Deploy your function only from firebase services to Google Cloud Platform.
+
+    ```bash
+    firebase deploy --only functions
+    ```
+
+## Additional Information
+
+- For more information about Firebase Cloud Functions, refer to the [Firebase Documentation](https://firebase.google.com/docs/functions).
---
+++ functions/index.js
@@ -0,0 +1,54 @@
+import { onRequest } from "firebase-functions/v2/https";
+import pkg from 'agora-token';
+
+const { RtcTokenBuilder, RtmTokenBuilder, RtcRole } = pkg;
+
+const AGORA_APP_ID = process.env.AGORA_APP_ID;
+const AGORA_CERTIFICATE = process.env.AGORA_CERTIFICATE;
+const TOKEN_EXPIRATION_IN_SECONDS = 3600;
+const INVALID_URL_FORMAT_MESSAGE = 'Invalid URL format';
+const INVALID_ENDPOINT_MESSAGE = 'Invalid endpoint';
+
+const agoraTokenAccess = onRequest(async (request, response) => {
+  const currentTimestamp = Math.floor(Date.now() / 1000);
+  const privilegeExpirationInSecond = currentTimestamp + TOKEN_EXPIRATION_IN_SECONDS;
+
+  if (request.url.includes("rtc")) {
+    const urlParts = request.url.split('/');
+    const channelNameIndex = urlParts.indexOf('rtc') + 1;
+    const uidIndex = urlParts.indexOf('uid') + 1;
+
+    if (channelNameIndex < urlParts.length && uidIndex < urlParts.length) {
+      const channelName = urlParts[channelNameIndex];
+      const uid = urlParts[uidIndex];
+      const role = RtcRole.PUBLISHER;
+      
+      const token = RtcTokenBuilder.buildTokenWithUid(
+        AGORA_APP_ID, AGORA_CERTIFICATE, channelName, uid, role, TOKEN_EXPIRATION_IN_SECONDS, privilegeExpirationInSecond
+      );
+
+      response.status(200).send({ "rtcToken": token });
+    } else {
+      response.status(400).send(INVALID_URL_FORMAT_MESSAGE);
+    }
+  } else if (request.url.includes("rtm")) {
+    const urlParts = request.url.split('/');
+    const rtmIdIndex = urlParts.indexOf('rtm') + 1;
+
+    if (rtmIdIndex > 0 && rtmIdIndex < urlParts.length) {
+      const userId = urlParts[rtmIdIndex];
+
+      const token = RtmTokenBuilder.buildToken(
+        AGORA_APP_ID, AGORA_CERTIFICATE, userId, privilegeExpirationInSecond
+      );
+
+      response.status(200).send({ "rtmToken": token });
+    } else {
+      response.status(400).send(INVALID_URL_FORMAT_MESSAGE);
+    }
+  } else {
+    response.status(400).send(INVALID_ENDPOINT_MESSAGE);
+  }
+});
+
+export { agoraTokenAccess };
